bec81d35473ab7c0623fd586b4585d0530c8e714
Jorge Kamezawa <jor_kamezawa@hotmail.com>
2023-06-30 01:04:27 -0300

    Curso 1 Modulo 2 finalizado

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo2'
+  }
+
+  criarPensamento() {
+    alert("Salvar")
+  }
+
+  cancelar() {
+    alert("Cancelar")
+  }
+}
